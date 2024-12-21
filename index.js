1bf78a07372f567efa6cd5eee555a633d0683304
shoyonp <shoyonkumar143@gmail.com>
2024-12-21 14:20:49 +0600

    token rremeove doe

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5174"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
@@ -65,11 +70,20 @@ async function run() {
     // auth related APIS
     app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "5h" });
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: false, // http://localhost:5173/
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: false,
         })
         .send({ success: true });
     });
